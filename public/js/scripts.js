d2abb8b1e06a9694b317c26c93e8b0b31c57a919
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-03-01 00:19:57 -0500

    custom donation code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -78,7 +78,7 @@ document.getElementById('donate1').addEventListener('click', function() {
   output.innerText = '';
     let sec = document.createElement('div'); sec.classList.add('don_sec');
     output.appendChild(sec);
-      let title = document.createElement('h4'); title.innerText = "Choose a Donation:";
+      let title = document.createElement('h4'); title.innerText = "Choose a Donation: ";
       output.appendChild(title);
       let form = document.createElement('form'); form.style.display = 'block';
         let don_op = ["$1", "$5", "$10", "$20", "$50", "$100"];
@@ -100,4 +100,24 @@ document.getElementById('donate2').addEventListener('click', function() {
   output.innerText = '';
     let sec = document.createElement('div'); sec.classList.add('don_sec');
     output.appendChild(sec);
+    let title = document.createElement('h4'); title.innerText = "Make a Custom Donation: ";
+    output.appendChild(title);
+      let donation = document.createElement('input'); donation.type = 'text'; donation.name = "custome_donation"; donation.id = 'custome_donation';
+      output.appendChild(donation);
+      let button = document.createElement('button'); button.innerHTML = 'GO'; button.id='don_button'
+      output.appendChild(button);
+      let warning = document.createElement("h5"); warning.classList.add('warning');
+      output.appendChild(warning);
+      document.getElementById('don_button').addEventListener('click', function() {
+        var don = document.getElementById('custome_donation').value;
+        // we will need to add try except or something here
+        var don = parseInt(don);
+        if (don < 1) {
+          warning.innerText = 'Your donation must be at lest $1.00!'
+        }else if (don > 500) {
+          warning.innerText = 'Your donation cannot be greater than $500.00!'
+        }else{
+          pass
+        }
+      });
 });
